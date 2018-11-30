1e576fe2b86f2aacb7730967db489b641c377674
Koozco <kussy.kusy@gmail.com>
2018-11-30 23:00:46 +0100

    pav ilp algorithm

--- pmp/rules/pav.py
+++ pmp/rules/pav.py
@@ -1,8 +1,10 @@
 from operator import itemgetter
 from six import iteritems
-from itertools import combinations
+from itertools import combinations, product, chain
+import numpy as np
 
 from .._common import solve_methods_registry
+from ..utils.ilp import *
 from .rule import Rule
 
 algorithm = solve_methods_registry()
@@ -25,10 +27,67 @@ class PAV(Rule):
         committee = algorithm.registry.all[method](self, k, profile)
         return committee
 
-    @algorithm('Bruteforce', 'Exponential.', default=True)
-    def brute(self, k, profile):
+    @algorithm('Bruteforce', 'Exponential.')
+    def _brute(self, k, profile):
         self.scores = self.compute_scores(k, profile)
         return max(iteritems(self.scores), key=itemgetter(1))[0]
+
+    @algorithm('ILP', default=True)
+    def _ilp(self, k, profile):
+        m = len(profile.candidates)
+        n = len(profile.preferences)
+        all_il = np.fromiter(chain.from_iterable(product(range(n), range(k))), int, n * k * 2)
+        all_il.shape = n * k, 2
+
+        model = Model()
+
+        # yi - ith candidate is in committee
+        y = ['y{}'.format(i) for i in range(m)]
+        y_lb = np.zeros(m)
+        y_ub = np.ones(m)
+        model.add_variables(y, y_lb, y_ub)
+
+        # xil - ith voter gets satisfaction for lth candidate he approves
+        x = ['x{}_{}'.format(i, l) for (i, l) in all_il]
+        x_lb = np.zeros(n * k)
+        x_ub = np.ones(n * k)
+        model.add_variables(x, x_lb, x_ub)
+
+        # Objective - alpha_l * x_i_l
+        model.set_objective_sense(Objective.maximize)
+        objective_weights = [self.alpha(l + 1) for (_, l) in all_il]
+        model.set_objective(x, objective_weights)
+
+        # Constraint1 - Vi Ei xi = k
+        # K candidates are chosen
+        yi = np.ones(m)
+        model.add_constraint(y, yi, Sense.eq, k)
+
+        # Constraint2 - Vi El xil <= E(i approves c) yc
+        # <=> Vi El xil - E(i approves c) yc <= 0
+        c2_variables = [
+            ['x{}_{}'.format(i, l) for l in range(k)] +
+            ['y{}'.format(profile.candidates.index(c)) for c in profile.preferences[i].approved]
+            for i in range(n)
+        ]
+        # coefficients for x's
+        ones = np.ones((n, k))
+        # coefficients for y's
+        minus_ones = np.full((n, k), -1)
+        c2_coefficients = np.concatenate((ones, minus_ones), axis=1)
+        c2_senses = np.full(n, Sense.lt)
+        c2_rights = np.zeros(n)
+        model.add_constraints(c2_variables, c2_coefficients, c2_senses, c2_rights)
+
+        # End of definition
+
+        model.solve()
+
+        solution = model.get_solution()
+        committee = (profile.candidates[i] for i in range(m) if solution['y{}'.format(i)] == 1)
+
+        self.scores[committee] = model.get_objective_value()
+        return committee
 
     def compute_scores(self, k, profile):
         scores = {}

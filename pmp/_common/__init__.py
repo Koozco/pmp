357a2c543c0e024b8350525739b8ea4ba890f7ce
Koozco <kussy.kusy@gmail.com>
2018-12-20 23:00:57 +0100

    default rule registry

--- pmp/_common/__init__.py
+++ pmp/_common/__init__.py
@@ -1,2 +1,2 @@
-from .methods import solve_methods_registry
+from .methods import solve_methods_registry, default_methods_registry
 from .intro import introspect_func
--- pmp/rules/rule.py
+++ pmp/rules/rule.py
@@ -1,8 +1,11 @@
 from .tie_breaking import random_winner
+from .._common import default_methods_registry
 
 
 class Rule:
     """Scoring rule."""
+
+    methods = default_methods_registry()
 
     def __init__(self, tie_break=random_winner):
         self.tie_break = tie_break

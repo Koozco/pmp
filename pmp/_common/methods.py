e84c668fdea53151ec03eaa700b7c53cc6f21512
Koozco <kussy.kusy@gmail.com>
2018-12-20 23:00:57 +0100

    default rule registry

--- pmp/_common/methods.py
+++ pmp/_common/methods.py
@@ -5,6 +5,14 @@ class MethodRegistry:
         self.default = None
 
 
+def default_methods_registry():
+    registry = MethodRegistry()
+    registry.all = {'find_committee': 'find_committee'}
+    registry.comments = {'find_committee': 'this rule has only one, default algorithm'}
+    registry.default = 'find_committee'
+    return registry
+
+
 def solve_methods_registry():
     registry = MethodRegistry()
 

699faf9ded46c3e05fb8fa64d27fd999b7bf155b
kussy.kusy@gmail.com <kussy.kusy@gmail.com>
2019-06-08 22:53:15 +0200

    release v1.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='pmp',
-      version='1.2.0',
+      version='1.2.1',
       description='Python Multiwinner Package',
       url='https://github.com/Koozco/pmp',
       author='Katarzyna Banaszak, Bartosz Kusek',
